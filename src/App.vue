9eabcf7cbbba58ad74635ee53bf226dc6e23523c
Matthew Brown <matthew.e.brown.17@gmail.com>
2022-03-05 04:48:19 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Anime Progress</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+
+  },
+});
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
