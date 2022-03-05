1463893f52073bcc7eaa57d951d1fb7d88bed587
Matthew Brown <matthew.e.brown.17@gmail.com>
2022-03-05 00:56:21 -0500

    Implement graphing of user progress

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
-import { getActivities, getMediaName, getSchedule, getUserId } from './data';
 import './style.scss';
+
+import { getActivities, getMediaName, getSchedule, getUserId } from './data';
+import {
+  Chart,
+  LineController,
+  LineElement,
+  PointElement,
+  CategoryScale,
+  LinearScale,
+  TimeScale,
+  Tooltip,
+} from 'chart.js';
+import 'chartjs-adapter-luxon';
 
 
 const body = {
@@ -7,6 +19,7 @@ const body = {
   canvas: document.querySelector<HTMLDivElement>('#canvas-wrapper')!,
   error: document.querySelector<HTMLDivElement>('#main-error')!,
 }
+
 
 const hide = (el: HTMLElement) => el.classList.add('hidden');
 const show = (el: HTMLElement) => el.classList.remove('hidden');
@@ -19,13 +32,71 @@ async function main(username: string, media: number) {
 
   console.log(activities);
 
-  if (activities.length) {
-    // Use the time from the very first status they have as the startTime
-    const startTime = activities[0].createdAt;
-    const schedules = await getSchedule(media, startTime);
-
-    console.log(schedules);
+  if (!activities.length) {
+    /// @TODO: Display error
+    return;
   }
+
+  // Use the time from the very first status they have as the startTime
+  const startTime = activities[0].createdAt;
+  const schedules = await getSchedule(media, startTime);
+
+  console.log(schedules);
+
+  if (!schedules.length) {
+    /// @TODO: Display error
+    return;
+  }
+
+  const activityData = activities
+    .map(activity => {
+      const x: Date = activity.createdAt;
+      const y: number = activity.progress !== null
+        ? Number(activity.progress.match(/\d+ - (\d+)/)?.[1])
+        : 0;
+
+      return activity.progress === null
+        ? { x, y, label: 'Added to plan-to-watch' }
+        : { x, y };
+    });
+
+  console.log(activityData);
+
+  const chart = new Chart(body.canvas.querySelector('canvas')!, {
+    type: 'line',
+    data: {
+      datasets: [
+        {
+          data: activityData,
+          label: 'Progress',
+          stepped: true,
+          borderColor: '#0061d1',
+          fill: '#0061d1',
+        }
+      ]
+    },
+    options: {
+      scales: {
+        x: { type: 'time' }
+      },
+      plugins: {
+        tooltip: {
+          callbacks: {
+            label: (context) => {
+              const point: any = context.dataset.data[context.dataIndex];
+              switch (context.datasetIndex) {
+                // If it's the activity progress
+                case 0:
+                  if (point.label) return point.label;
+                  else return `Watched up to episode ${context.formattedValue}`;
+                // If it's the show progress
+              }
+            }
+          }
+        }
+      }
+    }
+  });
 
 }
 
@@ -45,6 +116,7 @@ try {
   const parsedMedia = parseInt(envMedia);
   if (isNaN(parsedMedia)) throw new Error("Media ID is not a number");
 
+  // Add the title first
   await getMediaName(parsedMedia).then(({ english, native }) => {
     if (english == native) {
       body.title.innerText = native;
@@ -53,12 +125,29 @@ try {
     }
   });
 
+  // Setup chart.js
+  Chart.register(
+    LineController,
+    LineElement,
+    PointElement,
+    CategoryScale,
+    LinearScale,
+    TimeScale,
+    Tooltip,
+  );
+
   await main(envUser, parsedMedia);
 
 } catch (err: any) {
+
+  console.error(err);
 
   const pre = body.error.querySelector('pre');
   if (pre) pre.innerText = JSON.stringify(err, null, 2);
   show(body.error);
 
 }
+
+
+// for testing
+show(body.canvas);
